c5ee908633befdff22b7afcb8476f72865e99c99
Mauricio Garcia <fidel.garcia@globant.com>
2020-04-29 20:36:21 -0500

    css-loader style-loader

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ webpack runs node
 ways for run webpack:
 * on terminal: `npx webpack --entry ./index.js --output ./bundle.js --mode production `
 * on package.json: add a script. e.g. `"build": "webpack --mode production | development"`
+
+```
+npm i -D -E
+
+i = install
+-D = --save-dev
+-E = --save-exact
+```
 ___
 
 @mau2099 🐧
---
+++ css-style-loader/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'index.js'),
+
+  // mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader","css-loader"]
+      }
+    ],
+  },
+};
